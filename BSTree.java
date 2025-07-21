b213c81b1d6676698c90bd0c560108e8dd46beda
Umesh <yadavumeshkumar815@gmail.com>
2025-07-21 18:27:20 +0530

    done

---
+++ BSTree.java
@@ -0,0 +1,14 @@
+/*
+ * Binary Search tree->
+ * A Binary Search tree is a type of binary tree where each node has:
+ * 1). At most two children left and right 
+ * 2). The left subtree contains values with less than the current node vlaue.
+ * 3). THe right subtree contains only values with greater than current node values.\
+ * 4). Incase of Binary Search tree if we use in
+ */
+public class BSTree {
+    public static void main(String[]args)
+    {
+
+    }
+}
